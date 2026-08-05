33d2801f1a0ea3c23cb2c336be48d6e430ceef8b
IlonaChupa <ilonachupa@gmail.com>
2026-08-05 21:20:23 +0300

    add string constants

--- src/main/java/spring/test_task/service/impl/CategoryServiceImpl.java
+++ src/main/java/spring/test_task/service/impl/CategoryServiceImpl.java
@@ -20,12 +20,13 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
     private static final String NOT_FOUND_EXCEPTION_MESSAGE = "Category not found";
+    private static final String DUPLICATE_ENTITY_EXCEPTION_MESSAGE = "Category already exists";
 
     @Override
     @Transactional
     public CategoryDto createCategory(CreateCategoryRequestDto request) {
         if (categoryRepository.existsByName(request.getName())) {
-            throw new DuplicateEntityException("Category already exists");
+            throw new DuplicateEntityException(DUPLICATE_ENTITY_EXCEPTION_MESSAGE);
         } else {
             Category category = categoryRepository.save(categoryMapper.toEntity(request));
             return categoryMapper.toDto(category);
@@ -45,7 +46,7 @@ public class CategoryServiceImpl implements CategoryService {
     public CategoryDto getCategoryById(Long id) {
         return categoryMapper.toDto(categoryRepository
                 .findById(id)
-                .orElseThrow(()-> new EntityNotFoundException("Category not found")));
+                .orElseThrow(()-> new EntityNotFoundException(NOT_FOUND_EXCEPTION_MESSAGE)));
     }
 
     @Override
@@ -53,7 +54,7 @@ public class CategoryServiceImpl implements CategoryService {
     public CategoryDto updateCategory(Long id, CreateCategoryRequestDto request) {
         Category category = categoryRepository
                 .findById(id)
-                .orElseThrow(()-> new EntityNotFoundException("Category not found"));
+                .orElseThrow(()-> new EntityNotFoundException(NOT_FOUND_EXCEPTION_MESSAGE));
         categoryMapper.updateEntity(request, category);
         return categoryMapper.toDto(categoryRepository.save(category));
     }
@@ -64,7 +65,7 @@ public class CategoryServiceImpl implements CategoryService {
         if (categoryRepository.existsById(id)) {
             categoryRepository.deleteById(id);
         } else {
-            throw new EntityNotFoundException("Category not found");
+            throw new EntityNotFoundException(NOT_FOUND_EXCEPTION_MESSAGE);
         }
 
     }
