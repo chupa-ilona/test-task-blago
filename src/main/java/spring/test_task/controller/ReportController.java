fce9a0279f2c69039b9cf459e8eed95f69918c98
IlonaChupa <ilonachupa@gmail.com>
2026-08-07 16:42:24 +0300

    add swagger annotations to ReportController

--- src/main/java/spring/test_task/controller/ReportController.java
+++ src/main/java/spring/test_task/controller/ReportController.java
@@ -1,5 +1,7 @@
 package spring.test_task.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import spring.test_task.dto.MonthlyReportDto;
 import spring.test_task.service.ReportService;
 
+@Tag(name = "Reports management",
+        description = "Endpoints for generating financial reports")
 @RestController
 @RequestMapping("/api/reports")
 @RequiredArgsConstructor
@@ -16,6 +20,9 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping("/monthly")
+    @Operation(summary = "Get monthly financial report",
+            description = "Returns total income, total expenses,"
+                    + " balance, and expenses broken down by category for a specific month and year.")
     public MonthlyReportDto getMonthlyReport(
             @RequestParam int year,
             @RequestParam int month) {
